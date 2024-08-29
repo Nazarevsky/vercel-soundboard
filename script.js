e59f67a618e49a9100132f75964f3c593f57e868
nazarevsky <nazarevsky.ua@gmail.com>
2024-08-29 12:55:07 +0300

    Add more sounds, start, stop buttons and volume

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const soundSelect = document.getElementById('soundSelect');
     const soundPlayer = document.getElementById('soundPlayer');
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+    const volumeSlider = document.getElementById('volumeSlider');
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            const selectedSound = soundSelect.value;
-            soundPlayer.src = `${selectedSound}`;
-            soundPlayer.play();
+            playSound();
         }
     });
+
+    startButton.addEventListener('click', playSound);
+
+    stopButton.addEventListener('click', function() {
+        soundPlayer.pause();
+        soundPlayer.currentTime = 0;
+    });
+
+    volumeSlider.addEventListener('input', function() {
+        soundPlayer.volume = volumeSlider.value;
+    });
+
+    function playSound() {
+        const selectedSound = soundSelect.value;
+        soundPlayer.src = `${selectedSound}`;
+        soundPlayer.play();
+    }
 });
